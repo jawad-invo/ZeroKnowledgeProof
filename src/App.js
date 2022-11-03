f44d52e9d6386fd0bc739375f1dcef886bba6494
Jawad Akhtar <jawad.akhtar@invozone.com>
2022-11-03 17:01:12 +0500

    testapp clone

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import LandingPage from './components/pages/LandingPage'
-import LoginPage from './components/pages/LoginPage'
-import RegisterPage from './components/pages/RegisterPage'
-import ForgetPasswordPage from './components/pages/ForgetPasswordPage'
-import HomePage from './components/pages/HomePage'
+import LandingPage from "./components/pages/LandingPage";
+import LoginPage from "./components/pages/LoginPage";
+import HomePage from "./components/pages/HomePage";
 
-import './App.css'
-
-export default function App() {
-    return (
-        <Router>
-            <div>
-                <Switch>
-                    <Route exact path="/" component={ LandingPage } />
-                    <Route path="/login" component={ LoginPage } />
-                    {/* <Route path="/register" component={ RegisterPage } /> */}
-                    {/* <Route path="/forget-password" component={ ForgetPasswordPage } /> */}
-                    <Route path="/home" component={ HomePage } />
-                </Switch>
-                <Footer />
-            </div>
-        </Router>
-    )
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/home" element={<HomePage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 const Footer = () => {
-    return (
-        <p className="text-center" style={ FooterStyle }>Designed & coded by <a href="https://izemspot.netlify.com" target="_blank" rel="noopener noreferrer">InvoZone</a></p>
-    )
-}
+  return (
+    <p className="text-center" style={FooterStyle}>
+      Designed & coded by{" "}
+      <a
+        href="https://izemspot.netlify.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        InvoZone
+      </a>
+    </p>
+  );
+};
 
 const FooterStyle = {
-    background: "#222",
-    fontSize: ".8rem",
-    color: "#fff",
-    position: "absolute",
-    bottom: 0,
-    padding: "1rem",
-    margin: 0,
-    width: "100%",
-    opacity: ".5"
-}
+  background: "#222",
+  fontSize: ".8rem",
+  color: "#fff",
+  position: "absolute",
+  bottom: 0,
+  padding: "1rem",
+  margin: 0,
+  width: "100%",
+  opacity: ".5",
+};
+
+export default App;
